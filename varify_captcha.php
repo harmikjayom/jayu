d1cd940b04e6adfb8cf547e228b3c6bcd53abb70
Mathieu Sauvé <msauve@physiotec.org>
2022-10-05 10:48:40 -0400

    refactored

--- varify_captcha.php
+++ varify_captcha.php
@@ -1,16 +1,15 @@
 <?php
- 
+if(empty($_POST['g-recaptcha-response']))
+{
+  return;
+}
 
-  if(!empty($_POST['g-recaptcha-response']))
-  {
-        $secret = '6Lcax-MaAAAAAIn24kryOA6cUJ-48TflnXe9ApGG';
-        $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
-        $responseData = json_decode($verifyResponse);
-        
-        if($responseData->success)
-	        $message = "g-recaptcha varified successfully";
-        else
-	        $message = "Some error in vrifying g-recaptcha";
-	echo $message;
-   }
+$secret = '6Lcax-MaAAAAAIn24kryOA6cUJ-48TflnXe9ApGG';
+$verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+$responseData = json_decode($verifyResponse);
+
+$message = !empty($responseData->success) 
+              ? "g-recaptcha varified successfully";
+              : "Some error in vrifying g-recaptcha";
+echo $message;
 ?>
